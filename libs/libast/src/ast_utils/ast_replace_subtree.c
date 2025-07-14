65cd69be8d2f3c9b4b7a112d954c906bb60093d2
Paulina Lichota <plichota@student.42firenze.it>
2025-07-14 16:39:49 +0200

    norme

--- libs/libast/src/ast_utils/ast_replace_subtree.c
+++ libs/libast/src/ast_utils/ast_replace_subtree.c
@@ -3,26 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ast_replace_subtree.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sabruma <sabruma@student.42firenze.it>     +#+  +:+       +#+        */
+/*   By: plichota <plichota@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/21 22:46:12 by sabruma           #+#    #+#             */
-/*   Updated: 2025/07/10 17:03:41 by sabruma          ###   ########.fr       */
+/*   Updated: 2025/07/14 16:28:04 by plichota         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ast.h"
 
-// assuming both are trees (and therefore malloc'ed)
-// void	ast_replace_subtree(t_ast **target, t_ast *replace)
-// {
-// 	if (!target)
-// 		return ;
-// 	if (*target && *target != replace)
-// 		ast_free(*target);
-// 	*target = replace;
-// }
-
-void ast_replace_subtree(t_ast **target, t_ast *replace)
+void	ast_replace_subtree(t_ast **target, t_ast *replace)
 {
 	if (!target || *target == replace)
 		return;
